aca1ad375c9df194a6cf1caa7262ca30ec874572
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 10:49:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,11 +3,19 @@
 // including some code snippets below that you should find helpful
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
+
+$NewLink	=	'http://globalcontact.com/gc/directory/search.php?table=USDIR';
+$html 		= file_get_html($NewLink);
+echo $html;
+/*
+
+
+This loop is for number in page
 $alpha = array('a' , 'b');
-for($var = 1; $var<= 5; $var++) 
+for($var = 1; $var<= 2; $var++) 
 {
  echo $link = 'http://globalcontact.com/gc/directory/search.php?table=USDIR&company=a&search=&search_sic=&page=' .$var;
 }
-
+*/
 
 ?>
