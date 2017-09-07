ec4d9f6850fcda55df67ed03a6f6dfcabccf8317
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 13:15:37 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,12 +3,17 @@
 // including some code snippets below that you should find helpful
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
+echo "Hello World";
+
+/*
+
+
 $Baselink = 'http://globalcontact.com/gc/directory/search.php?table=USDIR&company=';
-$Alpha = array('a' , 'b')
+$Alpha = array('a' , 'b');
 
 
-/*This loop is for number in page*/
-;
+This loop is for number in page*/
+/*
 for($var = 0; $var <= sizeof($Alpha); $var++) 
 {
 $link =  $Baselink . $Alpha[$var] . '&search=&search_sic=&page=1';
@@ -19,34 +24,17 @@ if($html)
     if(is_object($lastpageno))
     {
     $totalpageloop =  preg_replace("/[^0-9]/","",$lastpageno);
+      echo $totalpageloop;
       
       
       
-      for($page = 0; $page <= 2; $page++)
-      {
-      $mainpage = $Baselink . $Alpha[$var] . '&search=&search_sic=&page=' .$page;
-        $link = = file_get_html($mainpage);
-        if(is_object($link)){
-        
-        foreach($link->find("/html/body/center/table/tbody/tr[2]/td[2]/div/div[2]/table/tbody/tr/td[2]/div/div[4]") as $element)
-        {
-        
-        $linkofprofile = $element->find("/html/body/center/table/tbody/tr[2]/td[2]/div/div[2]/table/tbody/tr/td[2]/div/div[4]/div[1]/table/tbody/tr[3]/td/div/table/tbody/tr/td[1]/a" 0)->href;
-        
-        echo $linkofprofile;
-        }
-        }
-      
-      }
-      
-      /*
     echo $int = filter_var($lastpageno, FILTER_SANITIZE_NUMBER_INT);
 
       
-        /*  $str = 'In My Cart : 11 12 items';
+          $str = 'In My Cart : 11 12 items';
     preg_match_all('!\d+!', $str, $matches);
       echo $matches;
-      */
+      
       
       //		echo		$checkloopnumberforpagintion	=	str_replace("<a href='search.php?table=USDIR&amp;company=", "", $lastpageno);
 
@@ -59,6 +47,6 @@ if($html)
  
  
 }
-
+*/
 
 ?>
