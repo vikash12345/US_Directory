ef5f4b6fb9ac2b7e7aacc6c1af2949147efbaf82
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 12:06:19 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,7 +15,7 @@ $link =  $Baselink . $Alpha[$var] . '&search=&search_sic=&page=1';
 $html 		= file_get_html($link);
 if($html)
   {
-     $lastpageno =  $html->find("/html/body/center/table/tbody/tr[2]/td[2]/div/div[2]/table/tbody/tr/td[2]/div/div[4]/center/div/a[11]", 0);
+     $lastpageno =  $html->find("/html/body/center/table/tbody/tr[2]/td[2]/div/div[2]/table/tbody/tr/td[2]/div/div[4]/center/div/a[11]", 0)->href;
     if(is_object($lastpageno))
     {
       			echo		$checkloopnumberforpagintion	=	str_replace("search.php?table=USDIR&company=", "", $lastpageno);
